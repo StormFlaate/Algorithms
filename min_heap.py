7b51691a62ce54ec86d328ad429e2a887b98e1c2
StormFlaate <fflaate@hotmail.no>
2022-06-16 14:56:42 +0200

    make sift_up and sift_down private functions

--- min_heap.py
+++ min_heap.py
@@ -1,26 +1,9 @@
+import heapq
 
-def heappush(heap: list[int], item: int) -> list[int]:
-    """
-    heappush will add a new item at the end/bottom of the heap (really just a list)
-    and then it will bump its way upward. It will stop if it is at the top (index 0)
-    or if the parent node is of bigger value.
-    """
-    heap.append(item)
-    n: int = len(heap)-1
-    while n > 0 and heap[(n-1)//2] > heap[n]:
-        heap[n] = heap[(n-1)//2]
-        heap[(n-1)//2] = item
-        n = (n-1)//2
-    
-    return heap
 
-def heappop(heap: list[int]) -> int:
-    if len(heap) == 1:
-        return heap.pop() 
-    min_val = heap[0]
-    heap[0] = heap.pop()
-    n: int = 0
-    while n < len(heap):
+def _sift_up(heap: list[int], startpos:int, endpos:int):
+    n: int = startpos
+    while n < endpos:
         switch = n
         if 2*n+1 < len(heap) and heap[2*n+1] < heap[switch]:
             switch = 2*n+1
@@ -32,6 +15,35 @@ def heappop(heap: list[int]) -> int:
         heap[n] = heap[switch]
         heap[switch] = tmp
         n = switch
+
+
+def _shift_down(heap: list[int], startpos:int, endpos:int):
+    n: int = startpos
+    while n > endpos and heap[(n-1)//2] > heap[n]:
+        tmp = heap[n]
+        heap[n] = heap[(n-1)//2]
+        heap[(n-1)//2] = tmp
+        n = (n-1)//2
+
+
+def heappush(heap: list[int], item: int) -> list[int]:
+    """
+    heappush will add a new item at the end/bottom of the heap (really just a list)
+    and then it will bump its way upward. It will stop if it is at the top (index 0)
+    or if the parent node is of bigger value.
+    """
+    heap.append(item)
+    _shift_down(heap, len(heap)-1, 0)
+    
+    return heap
+
+def heappop(heap: list[int]) -> int:
+    if len(heap) == 1:
+        return heap.pop() 
+    min_val = heap[0]
+    heap[0] = heap.pop()
+    _shift_up(heap, 0, len(heap))
+    
     return min_val
 
 
