3f2f056d922d12dc02f87cd4e7f66b215b23a0a1
StormFlaate <fflaate@hotmail.no>
2022-06-16 18:30:01 +0200

    Updating test_cases

--- test_min_heap.py
+++ test_min_heap.py
@@ -1,19 +1,18 @@
 import unittest
-from min_heap import heappop, heappush
+from min_heap import heapify, heappop, heappush
+import heapq
+import random
 
 class TestMinHeap(unittest.TestCase):
     def test_heappush(self):
-        # Test adding new elements to heap
-        test_heap_1 = []
-        test_heap_2 = [1, 3, 9, 7, 5]
-        for num in [2,3,4,7,6,5,1]:
-            heappush(test_heap_1, num)
-        heappush(test_heap_2, 4)
+        random.seed(10)
+        for i in range(10):
+            test_list = [random.randint(-100, 100) for i in range(10)]
+            ans_list = test_list[:]
+            heapq.heappush(ans_list, i)
+            heappush(test_list, i)
+            self.assertEqual(test_list, ans_list, "Error on heappush function")
         
-        
-        
-        self.assertEqual(test_heap_1, [1,3,2,7,6,5,4], "Error on heappush function")
-        self.assertEqual(test_heap_2, [1, 3, 4, 7, 5, 9], "Error on heappush function")
 
     def test_heappop(self):
         test_heap_1 = [1]
@@ -23,3 +22,14 @@ class TestMinHeap(unittest.TestCase):
         self.assertEqual(heappop(test_heap_1), 1)
         self.assertEqual(heappop(test_heap_2), 1)
         self.assertEqual(test_heap_2, [2,6])
+
+
+    def test_heapify(self):
+        random.seed(10)
+        for i in range(10):
+            test_list = [random.randint(-100, 100) for i in range(10)]
+            ans_list = test_list[:]
+            heapq.heapify(ans_list)
+            heapify(test_list)
+            self.assertEqual(test_list, ans_list, "Error on heapify function")
+
